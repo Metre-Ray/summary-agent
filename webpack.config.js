04623b98f44e5b6249efedaf6a03ccfeb41c1d56
metre-ray <dgary@tut.by>
2024-11-18 19:05:15 +0100

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,13 @@ module.exports = [
             filename: 'popup.js',
             path: path.resolve(__dirname, 'dist'),
         },
+    },
+    {
+        entry: './background.js',
+        output: {
+            filename: 'background.js',
+            path: path.resolve(__dirname, 'dist'),
+        },
         plugins: [
             new webpack.DefinePlugin({
                 'process.env.PDF_API_KEY': JSON.stringify(process.env.PDF_API_KEY),
